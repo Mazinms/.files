ab1013fdfb47195e9c122558be58d01fd2552ad1
Dan Van Boxel <the.dvb@gmail.com>
2013-11-26 19:05:38 -0500

    Minor adjustments for dev system like screensaver

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/zsh
 #source /home/vanboxel/.zshrc
+#open a terminal
 xterm &
+
+#start firefox browser
 firefox &
+
+#I don't remember what this one does
 xset b off
+
+#start the screensaver
+( ( sleep 10 && /usr/bin/xscreensaver -no-splash -display :0.0 ) & )
